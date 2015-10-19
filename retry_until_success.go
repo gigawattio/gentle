60e17cf0cc831d97d47c0431e8886071d140b6a5
Jay Taylor <outtatime@gmail.com>
2015-10-19 10:01:19 -0700

    Changed zk recursive deletion retry logic to apply to specific operations.
    Added extra call depth to logging in gently.RetryUntilSuccess.

--- retry_until_success.go
+++ retry_until_success.go
@@ -11,11 +11,15 @@ import (
 // Exponential backoff is used to prevent failing attempts from looping madly.
 func RetryUntilSuccess(name string, operation func() error, strategy backoff.BackOff) {
 	errNotifReceiver := func(err error, nextWait time.Duration) {
+		log.ExtraCalldepth = 1
 		log.Error("%s notified of error: %s [next wait=%s]", name, err, nextWait)
+		log.ExtraCalldepth = 0
 	}
 	for {
 		if err := backoff.RetryNotify(operation, strategy, errNotifReceiver); err != nil {
+			log.ExtraCalldepth = 1
 			log.Error("%s failure: %s [will keep trying]", name, err)
+			log.ExtraCalldepth = 0
 			strategy.Reset()
 			continue
 		}
