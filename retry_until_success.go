0392ec901b661c4bce630c9b63f9ad6da9376af4
Jay Taylor <outtatime@gmail.com>
2016-05-09 12:10:56 -0700

    Minor tweaks and clarifications for log output in `checkedrequest.Agent', `gentle.RetryUntilSucess', and `zk.util.CreateP' functions.

--- retry_until_success.go
+++ retry_until_success.go
@@ -12,13 +12,13 @@ import (
 func RetryUntilSuccess(name string, operation func() error, strategy backoff.BackOff) {
 	errNotifReceiver := func(err error, nextWait time.Duration) {
 		log.ExtraCalldepth = 1
-		log.Error("%s notified of error: %s [next wait=%s]", name, err, nextWait)
+		log.Error("%v notified of error: %s [next wait=%s]", name, err, nextWait)
 		log.ExtraCalldepth = 0
 	}
 	for {
 		if err := backoff.RetryNotify(operation, strategy, errNotifReceiver); err != nil {
 			log.ExtraCalldepth = 1
-			log.Error("%s failure: %s [will keep trying]", name, err)
+			log.Error("%v failure: %s [will keep trying]", name, err)
 			log.ExtraCalldepth = 0
 			strategy.Reset()
 			continue
